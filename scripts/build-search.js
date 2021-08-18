497ceb7a0d9f26cc2faa80e0ce0b760b490f926b
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-08-17 21:50:03 -0400

    started work for algolia search

---
+++ scripts/build-search.js
@@ -0,0 +1,11 @@
+/**
+ * Build the Algolia search index using the data folder
+*/
+
+import dotenv from "dotenv";
+
+// IIFE to use await
+(async function() {
+    // Init env vars
+    dotenv.config();
+})();
