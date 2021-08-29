df784ad21fd84b27db5985f8bea54d2b8ed2c34b
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-08-28 21:50:28 -0400

    fix: fixed formatting and error catching in summarize

--- scripts/summarize-grant.js
+++ scripts/summarize-grant.js
@@ -36,55 +36,55 @@ async function getAllGrantsAirtable() {
 // IIFE to use await
 (async function () {
     try {
-        console.log("Summarizing all grants...");
+        console.log("🔵 Summarizing all grants...");
 
         // Get all grants
         const allGrants = (await getAllGrantsAirtable());
 
         // Run all of the links through SMMRY
         const descriptionPromises = allGrants.map(async ({ name, link, objectID, description }) => {
-                if (description) {
-                    console.log(`🟡 Description already found, ignoring fetching for ${name}...`);
-                    return null;
-                }
+            if (description) {
+                console.log(`🟡 Description already found, ignoring fetching for ${name}...`);
+                return null;
+            }
 
-                // Run link through SMMRY
-                const res = await fetch(`https://api.smmry.com/&SM_API_KEY=${process.env.SMMRY_API_KEY}&SM_KEYWORD_COUNT=7&SM_WITH_BREAK&SM_URL=${link}`, {
-                    method: "POST",
-                })
+            // Run link through SMMRY
+            const res = await fetch(`https://api.smmry.com/&SM_API_KEY=${process.env.SMMRY_API_KEY}&SM_KEYWORD_COUNT=7&SM_WITH_BREAK&SM_URL=${link}`, {
+                method: "POST",
+            })
 
-                data = await res.json();
+            data = await res.json();
 
-                // Check data if everything went well
-                if ("sm_api_error" in data) {
-                    console.log(`❌ Error from API while fetching description for ${name}: ${data.sm_api_message}`);
-                    throw {
-                        error: data.sm_api_error,
-                        message: data.sm_api_message,
-                        data: data
-                    };
-                }
-
-                // Make all keywords lowercase
-                const keywords = data.sm_api_keyword_array.map((keyword) => keyword.toLowerCase())
-
-                // Change [BREAK] to \n in description
-                // One replace is run with a space, and one without, to remove any breaks at the end
-                const newDescription = data.sm_api_content.replace(/\[BREAK\] /g, "\n").replace(/\[BREAK\]/g, "\n");
-
-                // Set description and keywords of objectID from data
-                await grantsTable.update(objectID, {
-                    Description: newDescription,
-                    Keywords: keywords
-                }, {
-                    "typecast": true // Allows us to make new entries for the keywords
+            // Check data if everything went well
+            if ("sm_api_error" in data) {
+                console.log(`❌ Error from API while fetching description for ${name}: ${data.sm_api_message}`);
+                console.log("❌ More information: ", {
+                    error: data.sm_api_error,
+                    message: data.sm_api_message,
+                    data: data
                 });
+            }
 
-                // Let user know
-                console.log(`🎉 Successfully fetched keywords and description for ${name}`);
+            // Make all keywords lowercase
+            const keywords = data.sm_api_keyword_array.map((keyword) => keyword.toLowerCase())
+
+            // Change [BREAK] to \n in description
+            // One replace is run with a space, and one without, to remove any breaks at the end
+            const newDescription = data.sm_api_content.replace(/\[BREAK\] /g, "\n").replace(/\[BREAK\]/g, "\n");
+
+            // Set description and keywords of objectID from data
+            await grantsTable.update(objectID, {
+                Description: newDescription,
+                Keywords: keywords
+            }, {
+                "typecast": true // Allows us to make new entries for the keywords
             });
 
-            await Promise.all(descriptionPromises);
+            // Let user know
+            console.log(`🎉 Successfully fetched keywords and description for ${name}`);
+        });
+
+        await Promise.all(descriptionPromises);
     } catch (e) {
         console.log("Error when summarizing grants: ", e);
     }
