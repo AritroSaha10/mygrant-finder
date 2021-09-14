2504d7edc91c6d49d35a63f8ff287bc6a94166eb
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2021-09-14 19:46:23 -0400

    feat: added autocomplete with bugs

---
+++ components/Search/Autocomplete.tsx
@@ -0,0 +1,37 @@
+import { connectAutoComplete } from "react-instantsearch-core";
+
+function Autocomplete({ hits, currentRefinement, refine }) {
+    const autocompleteBoldingRange = (hit: string) => {
+        // Get the string with the smallest length
+        const smallestStringLength = currentRefinement.length > hit.length ? hit.length : currentRefinement.length;
+
+        let boldCount = 0;
+        for (let i = 0; i < smallestStringLength; i++) {
+            if (hit.toLocaleLowerCase()[i] === currentRefinement.toLocaleLowerCase()[i]) {
+                boldCount += 1;
+            } else {
+                break;
+            }
+        }
+
+        return boldCount;
+    };
+
+    return (
+        <ul className={hits.length && `flex flex-col gap-2 bg-white mt-2 rounded-lg`}>
+            {hits.map(hit => (
+                <li key={hit.objectID} onClick={() => refine(hit.name)} className="hover:bg-gray-100 px-4 py-1">
+                    <span>
+                        {hit.name.slice(0, autocompleteBoldingRange(hit.name))}
+                    </span>
+
+                    <span className="font-medium">
+                        {hit.name.slice(autocompleteBoldingRange(hit.name))}
+                    </span>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default connectAutoComplete(Autocomplete);
--- components/Search/SearchBox.tsx
+++ components/Search/SearchBox.tsx
@@ -1,27 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import { connectSearchBox } from "react-instantsearch-dom";
+import Autocomplete from "./Autocomplete";
 
-function SearchBox({ refine }) {
+function SearchBox({ currentRefinement, refine }) {
+  const [showAutocomplete, setShowAutocomplete] = useState(false);
+
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
   };
 
   return (
     <form action="" role="search" onSubmit={onSubmit}>
-      <div className="flex flex-col gap-1 flex-grow">
-        <label htmlFor="search">
-          <p className="text-sm text-gray-500 font-semibold">
-            Search
-          </p>
-        </label>
+      <div tabIndex={0} onFocus={e => { e.preventDefault(); setShowAutocomplete(true); }}
+          onBlur={e => setShowAutocomplete(false)} onClick={e => e.preventDefault()}>
+        <div className="flex flex-col gap-1 flex-grow">
+          <label htmlFor="search">
+            <p className="text-sm text-gray-500 font-semibold">
+              Search
+            </p>
+          </label>
 
-        <input
-          className="bg-gray-200 appearance-none outline-none px-4 py-1 rounded focus:ring focus:bg-gray-300 duration-75 w-full"
-          type="search"
-          placeholder="Ex. covid relief"
-          id="search"
-          onChange={(e) => refine(e.currentTarget.value)}
-        />
+          <input
+            className="bg-gray-200 appearance-none outline-none px-4 py-1 rounded focus:ring focus:bg-gray-300 duration-75 w-full"
+            type="search"
+            placeholder="Ex. covid relief"
+            id="search"
+            onChange={e => refine(e.currentTarget.value)}
+            value={currentRefinement}
+          />
+        </div>
+
+        {showAutocomplete && <Autocomplete />}
       </div>
     </form >
   )
